668b73777158f283d5ce89f3439c48a62e5412ab
s-weigand <s.weigand.phy@gmail.com>
2024-08-23 13:25:14 +0200

    ✨ Expose CLI via main module

--- openapi_diagram/__main__.py
+++ openapi_diagram/__main__.py
@@ -1,1 +1,8 @@
-"""Main module."""
+"""Entrypoint when calling ``python -m openapi_diagram``."""
+
+from __future__ import annotations
+
+from openapi_diagram.cli import app
+
+if __name__ == "__main__":
+    app()
